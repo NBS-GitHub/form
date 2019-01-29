7ebc8f53ea763bd8910108ef8248fa35daf479b9
Paweł Nobis <pawel.nobis1@gmail.com>
2019-01-29 14:40:34 +0100

    Updates

---
+++ precache-manifest.71d7649e1ec663a1ef9eed1b48a84da7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b74486485d5109e615be",
+    "url": "/form/static/js/runtime~main.b7448648.js"
+  },
+  {
+    "revision": "24e207617e3255996212",
+    "url": "/form/static/js/main.24e20761.chunk.js"
+  },
+  {
+    "revision": "d439e8744bee3815eb5d",
+    "url": "/form/static/js/1.d439e874.chunk.js"
+  },
+  {
+    "revision": "24e207617e3255996212",
+    "url": "/form/static/css/main.64c9018d.chunk.css"
+  },
+  {
+    "revision": "91cb37c30a73efb3cee41000b5b1a6fa",
+    "url": "/form/index.html"
+  }
+];
