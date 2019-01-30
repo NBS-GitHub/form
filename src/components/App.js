3b90df12c96b912bd7eb3955c0dde47bd48f8f8d
Paweł Nobis <pawel.nobis1@gmail.com>
2019-01-30 14:37:33 +0100

    buy ticket app

--- src/components/App.js
+++ src/components/App.js
@@ -1,72 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
-import Button from './Button';
-import Panel from './Panel';
+import Header from './Header';
+import Form from './Form';
+import Display from './Display';
+
 
 class App extends Component {
   state = {
-    count: 0,
-    result: 0
+    ageEnough: false,
+    isSubmitted: false
   }
 
-  handleClick(action, text) {
+  handleChange = () => {
     this.setState(prevState => ({
-      count: prevState.count + 1,
+      ageEnough: !prevState.ageEnough,
+      isSubmitted: false
     }));
-    if (action === 'subtraction') {
-      if (text === '-10') {
-        this.setState(prevState => ({
-          result: prevState.result - 10
-        }));
-      } else if (text === '-1') {
-        this.setState(prevState => ({
-          result: prevState.result - 1
-        }));
-      }
-    } else if (action === 'addition') {
-      if (text === '+1') {
-        this.setState(prevState => ({
-          result: prevState.result + 1
-        }));
-      } else if (text === '+10') {
-        this.setState(prevState => ({
-          result: prevState.result + 10
-        }));
-      }
-    } else if (action === 'reset') {
-      this.setState(prevState => ({
-        result: 0
-      }));
-    }
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState(prevState => ({
+      isSubmitted: true
+    }));
   }
 
   render() {
+    const { ageEnough, isSubmitted } = this.state;
     return (
       <>
-        <Button
-          click={this.handleClick.bind(this, 'subtraction', '-10')}
-          text='-10'
+        <Header />
+        <Form
+          change={this.handleChange}
+          submit={this.handleSubmit}
+          ageEnough={ageEnough}
         />
-        <Button
-          click={() => this.handleClick('subtraction', '-1')}
-          text='-1'
-        />
-        <Button
-          click={this.handleClick.bind(this, 'reset')}
-          text='Reset'
-        />
-        <Button
-          click={this.handleClick.bind(this, 'addition', '+1')}
-          text='+1'
-        />
-        <Button
-          click={this.handleClick.bind(this, 'addition', '+10')}
-          text='+10'
-        />
-        <Panel
-          count={this.state.count}
-          result={this.state.result}
-        />
+        <Display ageEnough={ageEnough} isSubmitted={isSubmitted} />
       </>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Buy a ticket for the horror of the year!!!</h1>
+        </header>
+    );
+}
+
+export default Header;
