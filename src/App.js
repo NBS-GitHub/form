3e0ad6960b458c80c8ce4c4fa4d4e87e736d0358
Paweł Nobis <pawel.nobis1@gmail.com>
2019-02-01 15:09:06 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, NavLink, Prompt, Redirect } from 'react-router-dom';
+import User from './components/User';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Error from './components/Error';
+import Navigation from './components/Navigation';
+
 
 class App extends Component {
+  state = {
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        {/* Navigation */}
+        <Router>
+          <>
+            <Navigation />
+            <Switch>
+              <Route path='/' exact strict component={Home}>Home</Route>
+              <Route path='/about' exact strict component={About}>About</Route>
+              <Route path='/contact' exact strict component={Contact}>Contact</Route>
+              <Route component={Error}></Route>
+            </Switch>
+          </>
+        </Router>
+
+        {/* Content */}
+        <article>
+          <h3>Article title</h3>
+          <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa optio, error quos cupiditate vel quidem nam velit modi suscipit? Nesciunt!</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore totam perferendis magni itaque porro debitis, iure placeat rerum error voluptates facilis unde, repellat, ipsum blanditiis sunt illo! Quos repellendus, ad amet consequuntur, aut ea modi ratione dolorem cupiditate rerum delectus ipsam tempore sint in nemo soluta obcaecati nobis vel iusto? Unde id ratione delectus sapiente vel, placeat ut quas eos exercitationem repellat, quia odio eligendi deserunt earum blanditiis modi laborum veritatis voluptatum tenetur eum, maxime aut? Id, libero saepe nostrum magnam, pariatur in ratione hic illum maiores sapiente quia a optio minus et nobis iure placeat ea, ad doloribus. Perferendis?</p>
+        </article>
+      </>
     );
   }
 }
 
 export default App;
+
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <>
+
+//         <Router>
+//           <>
+//             <Navigation />
+//             <Switch>
+//               <Route path='/' component={Home} exact />
+//               <Route path='/about' component={About} />
+//               <Route path='/contact' component={Contact} />
+//               <Route component={Error} />
+//             </Switch>
+//           </>
+//         </Router>
+
+//       </>
+//     );
+//   }
+// }
---
+++ src/components/User.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const User = params => <h1>Welcome User {params.username}</h1>;
+
+export default User;
